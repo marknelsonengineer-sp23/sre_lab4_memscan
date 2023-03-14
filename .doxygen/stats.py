703752aad945cf9c074edfcea1dcaf194b9a64c1
Mark Nelson <marknels@hawaii.edu>
2023-03-13 21:58:50 -1000

    Second refactoring of stats.py to fully document it and add it to Doxygen

--- .doxygen/stats.py
+++ .doxygen/stats.py
@@ -6,11 +6,14 @@
 #
 ## Generate project statistics
 ##
-## This code depends on the Git Python module.  To install it, login as Root:
+## This code depends on the Git Python module.  To install it, login as `root`:
 ## ````
 ##      # apt-get install python3-pip
 ##      # pip install GitPython
 ## ````
+##
+## @see https://github.com/gitpython-developers/GitPython
+## @see https://gitpython.readthedocs.io/en/stable/
 ##
 ## @file   stats.py
 ## @author Mark Nelson <marknels@hawaii.edu>
@@ -20,45 +23,86 @@ import sys
 import datetime
 import subprocess
 import git
-from git import Repo
 import pathlib
 import os
 import locale
 import re
 
+## All output is redirected to this file
+OUTPUT_FILE = "STATISTICS.md"
 
+## The GitHub username that owns this repository
 GITHUB_USERNAME = "marknelsonengineer-sp23"
+
+## The GitHub repository name
 GITHUB_REPO = "sre_lab4_memscan"
+
+## Use this style when printing content from shields_io
+##
+## @see https://shields.io
 STYLE = "social"
 
 
 locale.setlocale(locale.LC_ALL, '')  # Use '' for auto
 
-# Get	the current	Git Repository	(as a	global)
+## The current Git Repository (as a global)
 repo = git.Repo(".")
 
 # Initialize the sets of file	types	we	will process
+
+## The set of `.h` files from Git
 h_src_files = set()
+
+## The set of `.c` files from Git
 c_src_files = set()
+
+## The set of `.cpp` files from Git
 cpp_src_files = set()
+
+## The set of Boost Unit Test source files from Git
+##
+## @see https://www.boost.org/doc/libs/1_81_0/libs/test/doc/html/index.html
 unit_test_files = set()
+
+## The set of Python `.py` files from Git
 py_files = set()
+
+## The set of Web / HTML pages from Git
 web_files = set()
+
+## The set of Assembly Language source files from Git
 asm_files = set()
+
+## The set of Markdown `.md` files from Git
 markdown_files = set()
+
+## The set of image files from Git
 image_files = set()
+
+## The set of any other files in Git that are not in another set
 other_files = set()
 
-number_of_unit_tests = 0
-number_of_test_points = 0
+## The number of test cases #stats.unit_test_files
+number_of_test_cases = 0
+
+## The number of test assertions in #stats.unit_test_files
+number_of_test_assertions = 0
 
 
+## Print a title for #stats.OUTPUT_FILE
+##
+## @return Nothing
 def print_title():
 	print("Project Statistics")
 	print("==================")
 	print("")
 
 
+## Print the [Shields.io](https://shields.io) tags
+##
+## @see https://shields.io
+##
+## @return Nothing
 def print_shields_io_tags():
 	repo1 = f"![GitHub code size	in	bytes](https://img.shields.io/github/languages/code-size/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
 	repo2 = f"![GitHub repo size](https://img.shields.io/github/repo-size/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
@@ -81,6 +125,9 @@ def print_shields_io_tags():
 	print(f"| {repo1} <br/> {repo2} <br/> {repo3} | {commit1} <br/> {commit2} | {issue1} <br/> {issue2} | {social1} <br/> {social2} |")
 
 
+## Print a table of Git tags (if any)
+##
+## @return Nothing
 def print_tags():
 	# Use	the following to get	the tag/release history
 	# $ git tag	--sort=v:refname --format="%(tag)|%(contents)|%(creator)"
@@ -102,12 +149,20 @@ def print_tags():
 	print("")
 
 
+## Print `Automatically generated on` and the date
+##
+## @return Nothing
 def print_date():
 	print("")
 	now = datetime.datetime.now()
 	print("Automatically generated on " + now.strftime("%c"))
 
 
+## Find single-line comments (like `//` and `#`) and remove them
+##
+## @param line The line with a mix of working source and comments
+## @param sep The single-line comment delimiter
+## @return `line` stripped of any comments
 def remove_comments(line, sep="#"):
 	line_str = str(line, 'UTF-8')
 	i = line_str.find(sep)  # Find the position of the separator
@@ -116,6 +171,16 @@ def remove_comments(line, sep="#"):
 	return line_str.rstrip()
 
 
+## Process the set of shell-script files in `file_list` and report their statistics
+##   - Count the number of files
+##   - Count the total number of lines & working lines (no comments)
+##   - Count the number of bytes & working bytes (no comments)
+##
+## Working source is text that is stripped of content after a `#`
+##
+## @param row_name The label under the `Type` heading in the main statistics table
+## @param file_list The set of files to process
+## @return Nothing
 def process_script_files(row_name, file_list):
 	if len(file_list) == 0:
 		return
@@ -128,7 +193,7 @@ def process_script_files(row_name, file_list):
 	total_working_lines = 0
 	for f in file_list:
 		total_bytes += os.path.getsize(f)
-		total_lines += sum(1 for line in open(f, "rb"))
+		total_lines += sum(1 for _ in open(f, "rb"))
 		for line in open(f, "rb"):
 			line = remove_comments(line, "#")
 			if len(line) <= 0:
@@ -140,6 +205,16 @@ def process_script_files(row_name, file_list):
 	print(f"|{row_name}|{num_files:n}|{total_lines:n}|{total_bytes:n}|{total_working_lines:n}|{total_working_bytes:n}|")
 
 
+## Process the set of gcc files in `file_list` and report their statistics
+##   - Count the number of files
+##   - Count the total number of lines & working lines (no comments)
+##   - Count the number of bytes & working bytes (no comments)
+##
+## Use gcc's `-fpreprocessed` option to remove comments
+##
+## @param row_name The label under the `Type` heading in the main statistics table
+## @param file_list The set of files to process
+## @return Nothing
 def process_gcc_files(row_name, file_list):
 	if len(file_list) == 0:
 		return
@@ -152,7 +227,7 @@ def process_gcc_files(row_name, file_list):
 	total_working_lines = 0
 	for f in file_list:
 		total_bytes += os.path.getsize(f)
-		total_lines += sum(1 for line in open(f, "rb"))
+		total_lines += sum(1 for _ in open(f, "rb"))
 		result = subprocess.run(['gcc', '-fpreprocessed', '-dD', '-w', '-E', f], stdout=subprocess.PIPE)
 
 		all_output = ""
@@ -172,6 +247,14 @@ def process_gcc_files(row_name, file_list):
 	print(f"|{row_name}|{num_files:n}|{total_lines:n}|{total_bytes:n}|{total_working_lines:n}|{total_working_bytes:n}|")
 
 
+## Process the set of text files in `file_list` and report their statistics
+##   - Count the number of files
+##   - Count the number of lines
+##   - Count the number of bytes
+##
+## @param row_name The label under the `Type` heading in the main statistics table
+## @param file_list The set of files to process
+## @return Nothing
 def process_text_files(row_name, file_list):
 	if len(file_list) == 0:
 		return
@@ -182,11 +265,18 @@ def process_text_files(row_name, file_list):
 	total_lines = 0
 	for f in file_list:
 		total_bytes += os.path.getsize(f)
-		total_lines += sum(1 for line in open(f, "rb"))
+		total_lines += sum(1 for _ in open(f, "rb"))
 
 	print(f"|{row_name}|{num_files:n}|{total_lines:n}|{total_bytes:n}|n/a|n/a|")
 
 
+## Process the set of data files in `file_list` and report their statistics
+##   - Count the number of files
+##   - Count the number of bytes
+##
+## @param row_name The label under the `Type` heading in the main statistics table
+## @param file_list The set of files to process
+## @return Nothing
 def process_data_files(row_name, file_list):
 	if len(file_list) == 0:
 		return
@@ -200,12 +290,19 @@ def process_data_files(row_name, file_list):
 	print(f"|{row_name}|{num_files:n}|n/a|{total_bytes:n}|n/a|n/a|")
 
 
+## Print the main Project Statistics table.
+##   - For each file in the repository...
+##     - Classify the file into the appropriate `Type` using file extensions,
+##       folders, permissions, etc.
+##     - Process each file type and print a summary
+##
+## @return Nothing
 def process_files_in_git():
 	print("| Type | Files | Lines | Bytes | Working Lines | Working Bytes |")
 	print("|------|------:|------:|------:|--------------:|--------------:|")
 
-	global number_of_unit_tests
-	global number_of_test_points
+	global number_of_test_cases
+	global number_of_test_assertions
 
 	for (_path, _stage), entry in repo.index.entries.items():
 		# print( entry )
@@ -222,10 +319,10 @@ def process_files_in_git():
 			file = open(_path, "r")
 			for line in file:
 				if re.search("BOOST_.*_TEST_CASE", line):
-					number_of_unit_tests += 1
+					number_of_test_cases += 1
 					# print( line )
 				if re.search("BOOST_CHECK|BOOST_REQUIRE", line):
-					number_of_test_points += 1
+					number_of_test_assertions += 1
 					# print( line )
 
 		elif file_extension == '.h':
@@ -259,29 +356,38 @@ def process_files_in_git():
 	process_data_files("Other	Files", other_files)
 
 
+## Print the number of commits to the repo
+##
+## @return Nothing
 def print_number_of_commits():
 	print("")
 	number_of_commits = len(list(repo.iter_commits('HEAD')))
 	print(f"Number of commits:  {number_of_commits}")
 
 
+## Print the statistics related to Boost Tests in the repository
+##
+## @see https://www.boost.org/doc/libs/1_81_0/libs/test/doc/html/index.html
+##
+## @return Nothing
 def print_test_stats():
-	global number_of_unit_tests
-	global number_of_test_points
+	global number_of_test_cases
+	global number_of_test_assertions
 
-	if number_of_unit_tests == 0:
+	if number_of_test_cases == 0:
 		return
 
 	print("")
-	print(f"Number of unit tests:  {number_of_unit_tests}")
+	print(f"Number of test cases:  {number_of_test_cases}")
 	print("")
-	print(f"Number of test points:  {number_of_test_points}")
+	print(f"Number of test assertions:  {number_of_test_assertions}")
 
 
 # The	Main Program
 
-with open('STATISTICS.md',	'w') as redirected_output:
-	sys.stdout = redirected_output  # Redirect	stdout
+with open(OUTPUT_FILE,	'w') as redirected_output:
+	## Redirect `stdout`
+	sys.stdout = redirected_output
 	print_title()
 
 	process_files_in_git()
--- STATISTICS.md
+++ STATISTICS.md
@@ -6,26 +6,26 @@ Project Statistics
 |.h Source|11|533|24,405|188|4,703|
 |.c Source|9|1,984|74,511|1,270|38,673|
 |Unit Tests|4|575|30,687|413|24,696|
-|Python|3|462|12,863|263|8,848|
+|Python|3|568|16,414|263|8,868|
 |HTML|1|35|756|n/a|n/a|
 |Markdown|5|307|20,550|n/a|n/a|
 |Images|12|n/a|2,204,566|n/a|n/a|
-|Other	Files|9|n/a|140,430|n/a|n/a|
+|Other	Files|9|n/a|140,495|n/a|n/a|
 
 ## Tags
 No tags
 
 ## Software Engineering Metrics
 
-Number of commits:  119
+Number of commits:  121
 
-Number of unit tests:  15
+Number of test cases:  15
 
-Number of test points:  312
+Number of test assertions:  312
 
 ## GitHub Statistics
 | Repository                           | Commits                   | Issues                  | Social                    |
 |--------------------------------------|---------------------------|-------------------------|---------------------------|
 | ![GitHub code size	in	bytes](https://img.shields.io/github/languages/code-size/marknelsonengineer-sp23/sre_lab4_memscan?style=social) <br/> ![GitHub repo size](https://img.shields.io/github/repo-size/marknelsonengineer-sp23/sre_lab4_memscan?style=social) <br/> ![GitHub contributors](https://img.shields.io/github/contributors/marknelsonengineer-sp23/sre_lab4_memscan?style=social) | ![GitHub commit activity](https://img.shields.io/github/commit-activity/w/marknelsonengineer-sp23/sre_lab4_memscan?style=social) <br/> ![GitHub last	commit](https://img.shields.io/github/last-commit/marknelsonengineer-sp23/sre_lab4_memscan?style=social) | ![GitHub	issues](https://img.shields.io/github/issues-raw/marknelsonengineer-sp23/sre_lab4_memscan?style=social) <br/> ![GitHub	closed issues](https://img.shields.io/github/issues-closed-raw/marknelsonengineer-sp23/sre_lab4_memscan?style=social) | ![GitHub forks](https://img.shields.io/github/forks/marknelsonengineer-sp23/sre_lab4_memscan?style=social) <br/> ![GitHub Repo	stars](https://img.shields.io/github/stars/marknelsonengineer-sp23/sre_lab4_memscan?style=social) |
 
-Automatically generated on Mon 13 Mar 2023 04:29:56 PM 
+Automatically generated on Mon 13 Mar 2023 09:55:38 PM 
