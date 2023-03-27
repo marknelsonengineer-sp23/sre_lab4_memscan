630b1229a0125d570709f6fc8303f6e6a3aed0e3
Mark Nelson <marknels@hawaii.edu>
2023-03-26 17:07:07 -1000

    Issue #55:  Slowly migrate maps to a testable framework... Start by moving struct MapEntry to maps.h

--- maps.c
+++ maps.c
@@ -39,32 +39,6 @@ char* ExcludePaths[] = { "[vvar]"
                        } ;
 
 
-/// Hold the original (and some processed data) from each line of
-/// `/proc/$PID/maps`
-struct MapEntry {
-   char   szLine[MAX_LINE_LENGTH] ; ///< String buffer for the entire line.
-                                    ///  All of the string pointers in this
-                                    ///  struct will point to strings in this
-                                    ///  buffer after they've been tokenized
-   char*  sAddressStart ;   ///< String pointer to the start of the address range
-   char*  sAddressEnd ;     ///< String pointer to the end of the address range
-   void*  pAddressStart ;   ///< Pointer to the start of the memory mapped region
-   void*  pAddressEnd ;     ///< Pointer to the byte just *after* the end of
-                            ///  the memory mapped region
-   char*  sPermissions ;    ///< String pointer to the permissions
-   char*  sOffset ;         ///< String pointer to the offset
-   char*  sDevice ;         ///< String pointer to the device name
-   char*  sInode ;          ///< String pointer to the iNode number
-   char*  sPath ;           ///< String pointer to the path (may be `NULL`)
-   bool   include ;         ///< `true` if the entry should be processed.  `false` if it should be excluded.
-   size_t numBytes ;        ///< Number of bytes in this region
-   size_t numPages ;        ///< The number of #PageInfo records allocated for this region
-   struct PageInfo* pages ; ///< Pointer to a #PageInfo array
-   size_t numBytesFound ;   ///< Number of #byteToScanFor bytes found in this region when #scanForByte is set
-   double shannonEntropy ;  ///< Shannon Entropy of the region when #scanForShannon is set
-} ;
-
-
 /// Holds up to #MAX_ENTRIES of #MapEntry structs
 struct MapEntry map[MAX_ENTRIES] ;
 
