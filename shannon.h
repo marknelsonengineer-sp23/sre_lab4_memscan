42b7a422921ce104b11b522cc868acb67ab2c5c3
Mark Nelson <marknels@hawaii.edu>
2023-04-03 08:37:44 -1000

    Closeout an old @todo in shannon.h.

--- shannon.h
+++ shannon.h
@@ -4,6 +4,9 @@
 //
 /// Compute the Shannon Entropy of various buffers
 ///
+/// @see https://en.wikipedia.org/wiki/Entropy_(information_theory)
+/// @see https://rosettacode.org/wiki/Entropy#C
+///
 /// @file   shannon.h
 /// @author Mark Nelson <marknels@hawaii.edu>
 ///////////////////////////////////////////////////////////////////////////////
@@ -12,6 +15,7 @@
 
 /// The maximum length of a Shannon Entropy classification string
 #define MAX_SHANNON_CLASSIFICATION_LENGTH 24
+
 
 /// Compute the Shannon Entropy for `buffer`
 ///
@@ -23,8 +27,9 @@
 /// @return The Shannon Entropy (from 0.0 to 8.0 )
 extern double computeShannonEntropy( const void* buffer, const size_t length ) ;
 
+
 /// Lookup `entropy` and make a guess (classify it) as to what the data represents
 ///
 /// @param entropy The Shannon Entropy of a buffer
-/// @return @todo
+/// @return A string a la `English text in UNICODE`, `x86 code`, et al.
 extern char* getShannonClassification( const double entropy ) ;
