0d1d81cb0690b2a2054a10163e41af74a40385b6
Mark Nelson <marknels@hawaii.edu>
2023-04-27 15:26:48 -1000

    In test_iomem.cpp   - Add spacing   - Improve tests

--- tests/test_iomem.cpp
+++ tests/test_iomem.cpp
@@ -32,6 +32,8 @@ extern "C" bool validate_iomem() ;
 extern "C" void add_iomem_region( const_pfn_t start, const_pfn_t end, const char* description ) ;
 extern "C" void compose_iomem_summary() ;
 extern "C" void sort_iomem_summary() ;
+extern "C" void print_iomem_summary() ;
+
 // extern "C" void print_iomem_summary() ;
 
 /* ***************************************************************************/
@@ -73,23 +75,33 @@ BOOST_FIXTURE_TEST_CASE( test_iomem_release, TestIomemFixture ) {
    BOOST_CHECK_EQUAL( get_iomem_region_description( 0x00000000000 ), UNMAPPED_MEMORY_DESCRIPTION ) ;
    BOOST_CHECK_EQUAL( get_iomem_region_description( MAX_PHYS_ADDR ), UNMAPPED_MEMORY_DESCRIPTION ) ;
    BOOST_CHECK( validate_iomem() ) ;
+
+   BOOST_CHECK_NO_THROW( compose_iomem_summary() ) ;
+   BOOST_CHECK_NO_THROW( sort_iomem_summary() ) ;
+   //BOOST_CHECK_NO_THROW( print_iomem_summary() ) ;
+
    BOOST_CHECK_NO_THROW( release_iomem() ) ;
+
    BOOST_CHECK_EQUAL( get_iomem_region_description( 0x00000000000 ), UNMAPPED_MEMORY_DESCRIPTION ) ;
    BOOST_CHECK_EQUAL( get_iomem_region_description( MAX_PHYS_ADDR ), UNMAPPED_MEMORY_DESCRIPTION ) ;
    BOOST_CHECK( validate_iomem() ) ;
+
+   BOOST_CHECK_NO_THROW( compose_iomem_summary() ) ;
+   BOOST_CHECK_NO_THROW( sort_iomem_summary() ) ;
+   //BOOST_CHECK_NO_THROW( print_iomem_summary() ) ;
 }
 
 
 BOOST_FIXTURE_TEST_CASE( test_iomem_add, TestIomemFixture ) {
-   BOOST_CHECK_EQUAL( get_iomem_region_description(         0x000 ), UNMAPPED_MEMORY_DESCRIPTION ) ;
-   BOOST_CHECK_EQUAL( get_iomem_region_description(         0x100 ), UNMAPPED_MEMORY_DESCRIPTION ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x00000000000 ), UNMAPPED_MEMORY_DESCRIPTION ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x7ffffffffff ), UNMAPPED_MEMORY_DESCRIPTION ) ;
    BOOST_CHECK_EQUAL( get_iomem_region_description( MAX_PHYS_ADDR ), UNMAPPED_MEMORY_DESCRIPTION ) ;
 
-   BOOST_CHECK_NO_THROW( add_iomem_region( 0, MAX_PHYS_ADDR, "replaced - all") ) ;
+   BOOST_CHECK_NO_THROW( add_iomem_region( 0x00, MAX_PHYS_ADDR, "replaced - all") ) ;
    BOOST_CHECK( validate_iomem() ) ;
 
-   BOOST_CHECK_EQUAL( get_iomem_region_description(         0x000 ), "replaced - all" ) ;
-   BOOST_CHECK_EQUAL( get_iomem_region_description(         0x100 ), "replaced - all" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x00000000000 ), "replaced - all" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x7ffffffffff ), "replaced - all" ) ;
    BOOST_CHECK_EQUAL( get_iomem_region_description( MAX_PHYS_ADDR ), "replaced - all" ) ;
 
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x000, 0x0FF, "region 0 - new") ) ;
@@ -97,20 +109,82 @@ BOOST_FIXTURE_TEST_CASE( test_iomem_add, TestIomemFixture ) {
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x200, 0x2FF, "region 2 - new") ) ;
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x300, 0x3FF, "region 3 - new") ) ;
 
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x000 ), "region 0 - new" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x0FF ), "region 0 - new" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x100 ), "region 1 - new" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x1FF ), "region 1 - new" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x200 ), "region 2 - new" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x2FF ), "region 2 - new" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x300 ), "region 3 - new" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x3FF ), "region 3 - new" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x400 ), "replaced - all" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( MAX_PHYS_ADDR ), "replaced - all" ) ;
+
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x000, 0x0FF, "region 0") ) ;
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x100, 0x1FF, "region 1") ) ;
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x200, 0x2FF, "region 2") ) ;
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x300, 0x3FF, "region 3") ) ;
+
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x000 ), "region 0" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x0FF ), "region 0" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x100 ), "region 1" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x1FF ), "region 1" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x200 ), "region 2" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x2FF ), "region 2" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x300 ), "region 3" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x3FF ), "region 3" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x400 ), "replaced - all" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( MAX_PHYS_ADDR ), "replaced - all" ) ;
 
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x060, 0x0AF, "region 0 - inside") ) ;
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x160, 0x1AF, "region 1 - inside") ) ;
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x260, 0x2AF, "region 2 - inside") ) ;
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x360, 0x3AF, "region 3 - inside") ) ;
 
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x000 ), "region 0" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x059 ), "region 0" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x060 ), "region 0 - inside" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x0AF ), "region 0 - inside" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x0B0 ), "region 0" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x0FF ), "region 0" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x100 ), "region 1" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x159 ), "region 1" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x160 ), "region 1 - inside" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x1AF ), "region 1 - inside" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x1B0 ), "region 1" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x1FF ), "region 1" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x200 ), "region 2" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x259 ), "region 2" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x260 ), "region 2 - inside" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x2AF ), "region 2 - inside" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x2B0 ), "region 2" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x2FF ), "region 2" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x300 ), "region 3" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x359 ), "region 3" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x360 ), "region 3 - inside" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x3AF ), "region 3 - inside" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x3B0 ), "region 3" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x3FF ), "region 3" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x400 ), "replaced - all" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( MAX_PHYS_ADDR ), "replaced - all" ) ;
+
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x000, 0x02F, "region 0 - left") ) ;
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x100, 0x12F, "region 1 - left") ) ;
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x200, 0x22F, "region 2 - left") ) ;
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x300, 0x32F, "region 3 - left") ) ;
+
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x000 ), "region 0 - left" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x02F ), "region 0 - left" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x030 ), "region 0" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x100 ), "region 1 - left" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x12F ), "region 1 - left" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x130 ), "region 1" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x200 ), "region 2 - left" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x22F ), "region 2 - left" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x230 ), "region 2" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x300 ), "region 3 - left" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x32F ), "region 3 - left" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x330 ), "region 3" ) ;
 
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x0D0, 0x0FF, "region 0 - right") ) ;
    BOOST_CHECK_NO_THROW( add_iomem_region( 0x1D0, 0x1FF, "region 1 - right") ) ;
@@ -180,27 +254,47 @@ BOOST_FIXTURE_TEST_CASE( test_iomem_add, TestIomemFixture ) {
    BOOST_CHECK_EQUAL( get_iomem_region_description( 0x3d0 ), "region 3 - right" ) ;
    BOOST_CHECK_EQUAL( get_iomem_region_description( 0x3e0 ), "region 3 - right" ) ;
    BOOST_CHECK_EQUAL( get_iomem_region_description( 0x3ff ), "region 3 - right" ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x400 ), "replaced - all" ) ;
 
    BOOST_CHECK_EQUAL( get_iomem_region_description( MAX_PHYS_ADDR ), "replaced - all" ) ;
 
    BOOST_CHECK( validate_iomem() ) ;
+
+   BOOST_CHECK_NO_THROW( compose_iomem_summary() ) ;
+   BOOST_CHECK_NO_THROW( sort_iomem_summary() ) ;
+   //BOOST_CHECK_NO_THROW( print_iomem_summary() ) ;
+
    BOOST_CHECK_NO_THROW( release_iomem() ) ;
    BOOST_CHECK( validate_iomem() ) ;
 
-   BOOST_CHECK_EQUAL( get_iomem_region_description(         0x000 ), UNMAPPED_MEMORY_DESCRIPTION ) ;
-   BOOST_CHECK_EQUAL( get_iomem_region_description(         0x100 ), UNMAPPED_MEMORY_DESCRIPTION ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x00000000000 ), UNMAPPED_MEMORY_DESCRIPTION ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x7ffffffffff ), UNMAPPED_MEMORY_DESCRIPTION ) ;
    BOOST_CHECK_EQUAL( get_iomem_region_description( MAX_PHYS_ADDR ), UNMAPPED_MEMORY_DESCRIPTION ) ;
 } // test_iomem_add
 
 
 BOOST_FIXTURE_TEST_CASE( test_iomem_add_bad, TestIomemFixture ) {
-   BOOST_CHECK_FAIL( add_iomem_region( 0x000, 0x000, "test" ) ) ;
-   BOOST_CHECK_FAIL( add_iomem_region( 0x001, 0x000, "test" ) ) ;
-   BOOST_CHECK_FAIL( add_iomem_region( 0x000, 0x001, NULL ) ) ;
-   BOOST_CHECK_FAIL( add_iomem_region( 0x000, 0x001, "" ) ) ;
+   BOOST_CHECK_FAIL( add_iomem_region( 0x000, 0x000, "Map a region that ends at 0" ) ) ;
+   BOOST_CHECK_FAIL( add_iomem_region( 0x001, 0x000, "Map a region where start > end at the extreme" ) ) ;
+
+   BOOST_CHECK_FAIL( add_iomem_region( 0x80000000000, 0x7ffffffffff, "Map a region where start > end" ) ) ;
+
+   BOOST_CHECK_FAIL( add_iomem_region( MAX_PHYS_ADDR, MAX_PHYS_ADDR-1, "Map a region where start > end at the extreme" ) ) ;
+   BOOST_CHECK_FAIL( add_iomem_region( MAX_PHYS_ADDR, 0x00000000000, "Map a region that are opposite to the extreme" ) ) ;
+
+   BOOST_CHECK_FAIL( add_iomem_region( 0x000, 0x001, NULL ) ) ;  // Map a region with a NULL description
+   BOOST_CHECK_FAIL( add_iomem_region( 0x000, 0x001, "" ) ) ;  // Map a region with an empty description
    // @TODO Add some 1 byte regions (they should fail)
 
    BOOST_CHECK( validate_iomem() ) ;
+}
+
+BOOST_FIXTURE_TEST_CASE( test_iomem_add_description, TestIomemFixture ) {
+   BOOST_CHECK_NO_THROW( add_iomem_region( 0x000, 0x001, "  A  " ) ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x00 ), "  A  " ) ;
+
+   BOOST_CHECK_NO_THROW( add_iomem_region( 0x000, 0x001, "  A  B  " ) ) ;
+   BOOST_CHECK_EQUAL( get_iomem_region_description( 0x00 ), "  A  B  " ) ;
 }
 
 BOOST_FIXTURE_TEST_CASE( test_iomem_add_overlap, TestIomemFixture ) {
